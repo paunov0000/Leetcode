7235a66d79387797928d0f2b0108cf66084fb1cc
paunov0000 <legendarygriefer96@gmail.com>
2023-08-21 16:27:25 +0300

    Solved Problem03

---
+++ ArraysAndHashing/Problem03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndHashing
+{
+    public class Problem03
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var x = target - nums[i];
+
+                if (map.ContainsKey(x))
+                {
+                    return new int[] { map[x], i };
+                }
+
+                map[nums[i]] = i;
+            }
+
+            return new int[0];
+        }
+    }
+}
