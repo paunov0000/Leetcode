da159b6c06422d07fafa081bd911d39cf91433f9
paunov0000 <legendarygriefer96@gmail.com>
2023-08-21 15:30:50 +0300

    Solved 2 problems from Arrays & Hashing

---
+++ ArraysAndHashing/Problem02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndHashing
+{
+    public class Problem02
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            var chars = new int[128];
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            foreach (char c in t)
+            {
+                chars[c]++;
+            }
+
+            foreach (char c in s)
+            {
+                if (chars[c] != 0)
+                {
+                    chars[c]--;
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- ArraysAndHashing/Program.cs
+++ ArraysAndHashing/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ArraysAndHashing;
 
-var problem01 = new Problem01();
-var result = problem01.ContainsDuplicate(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+//var problem01 = new Problem01();
+//var result = problem01.ContainsDuplicate(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+var problem02 = new Problem02();
+var result = problem02.IsAnagram("a", "ab");
 
 Console.WriteLine(result);
