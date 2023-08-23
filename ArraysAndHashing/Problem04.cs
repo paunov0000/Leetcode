bfdf0391fa73fa1cfcc96526adda30f4ec2a2d46
paunov0000 <legendarygriefer96@gmail.com>
2023-08-23 12:58:40 +0300

    solved problem 04

---
+++ ArraysAndHashing/Problem04.cs
@@ -0,0 +1,33 @@
+﻿namespace ArraysAndHashing
+{
+    public class Problem04
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var result = new Dictionary<string, IList<string>>();
+
+            foreach (var str in strs)
+            {
+                var count = new char[26];
+
+                foreach (var c in str)
+                {
+                    count[c - 'a']++;
+                }
+
+                var key = new string(count);
+
+                if (!result.ContainsKey(key))
+                {
+                    result.Add(key, new List<string> { str });
+                }
+                else
+                {
+                    result[key].Add(str);
+                }
+            }
+
+            return result.Values.ToList();
+        }
+    }
+}
--- ArraysAndHashing/Program.cs
+++ ArraysAndHashing/Program.cs
@@ -1,9 +1,23 @@
 ﻿using ArraysAndHashing;
+using System.Text;
 
 //var problem01 = new Problem01();
 //var result = problem01.ContainsDuplicate(Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
 
-var problem02 = new Problem02();
-var result = problem02.IsAnagram("a", "ab");
+//var problem02 = new Problem02();
+//var result = problem02.IsAnagram("a", "ab");
 
-Console.WriteLine(result);
+//var problem04 = new Problem04();
+//var result = problem04.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+//var sb = new StringBuilder();
+
+//foreach (var item in result)
+//{
+//    foreach (var item2 in item)
+//    {
+//        sb.AppendLine(item2);
+//    }
+//}
+
+//Console.WriteLine(sb.ToString());
